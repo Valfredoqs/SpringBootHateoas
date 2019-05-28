5d33232a11608e7dcb0c4f71e90501aeca223a68
Valfredoqs <valfredosilva@gmail.com>
2019-05-27 22:17:26 -0300

    hateoas

--- src/main/java/br/senaigo/mobile/controlller/OrderController.java
+++ src/main/java/br/senaigo/mobile/controlller/OrderController.java
@@ -13,136 +13,103 @@ import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.senaigo.mobile.controlller.abstracts.PadraoController;
 import br.senaigo.mobile.entities.Order;
-import br.senaigo.mobile.interfaces.GenericOperationsController;
+import br.senaigo.mobile.interfaces.GenericOperations;
 import br.senaigo.mobile.service.OrderService;
-/**
- * 
- * @author bruno
- *
- */
+
+
 @RestController("/orders")
-public class OrderController implements GenericOperationsController<Order> {
-	
+public class OrderController extends PadraoController<Order> {
+
 	/**
 	 * @see http://appsdeveloperblog.com/spring-boot-logging-with-loggerfactory/
 	 */
 	Logger logger = LoggerFactory.getLogger(OrderController.class);
-	
+
 	@Autowired
 	public OrderService orderService;
-	
+
 	@Override
-	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
-				 produces = {MediaType.APPLICATION_JSON_VALUE,
-						 	 MediaType.APPLICATION_XML_VALUE,
-						 	 MediaTypes.HAL_JSON_VALUE})
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaTypes.HAL_JSON_VALUE })
 	@ResponseStatus(HttpStatus.CREATED)
-	public Resource<Order> post(@RequestBody Order order) {
+	public ResponseEntity<Resource<Order>> post(@RequestBody Order order) {
 		try {
 			orderService.post(order);
-			logger.info("Registro inserido");
-			
+			getLogger().info("Registro inserido");
+
 			Link link = linkTo(OrderController.class).slash(order.getId()).withSelfRel();
-			Resource<Order> result = new Resource<Order>(order,link);
-			return result;
+			Resource<Order> result = new Resource<Order>(order, link);
+			return ResponseEntity.ok(result);
 		} catch (Exception e) {
-			logger.error(String.format("Erro ao executar o método POST.\nMensagem: %s",e.getMessage()));
-		}
-		return null;
-	}
-
-	@Override
-	@PutMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void put(@RequestBody Order order) {
-		try {
-			orderService.put(order);
-			logger.info(String.format("Registro atualizado: %s",order.toString()));
-		} catch (Exception e) {
-			logger.error(String.format("Erro ao executar o método PUT.\nMensagem: %s",e.getMessage()));
+			getLogger().error(String.format("Erro ao executar o método POST.\nMensagem: %s", e.getMessage()));
+			return ResponseEntity.badRequest().body(null);
 		}
 	}
 
 	@Override
-	@DeleteMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@RequestBody Order orders) {
-		try {
-			orderService.delete(orders);
-			logger.info(String.format("Registro(s) deletado(s): %s",orders.toString()));
-		} catch (Exception e) {
-			logger.error(String.format("Erro ao executar o método PUT.\nMensagem: %s",e.getMessage()));
-		}
-
-	}
-
-	@Override
-	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, 
-							 MediaType.APPLICATION_XML_VALUE,
-							 MediaTypes.HAL_JSON_VALUE})
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
+			MediaTypes.HAL_JSON_VALUE })
 	@ResponseStatus(HttpStatus.OK)
-	public Resources<Order> get() {
+	public ResponseEntity<Resources<Order>> get() {
 		try {
 			List<Order> orders = orderService.get();
-			
-			logger.info(String.format("Registro(s) recuperados(s): %s",orders.toString()));
-			
-//			for (final Order order : orders) {
-//		        Link selfLink = linkTo(methodOn(OrderController.class)
-//		          .getOrderById(customerId, order.getIdOrder()())).withSelfRel();
-//		        order.add(selfLink);
-//		    }
-//		  
-//		    Link link = linkTo(methodOn(CustomerController.class)
-//		      .getOrdersForCustomer(customerId)).withSelfRel();
-//		    Resources<Order> result = new Resources<Order>(orders, link);
-//		    return result;
-			
+
+			getLogger().info(String.format("Registro(s) recuperados(s): %s", orders.toString()));
+
+			for (final Order order : orders) {
+				Link selfLink = linkTo(OrderController.class).slash(order.getIdOrder()).withSelfRel();
+
+				Link selfLinkPeople = linkTo(PeopleController.class).slash(order.getPeople().getIdPeople())
+						.withSelfRel();
+
+				order.getPeople().add(selfLinkPeople);
+				order.add(selfLink);
+			}
+			Link link = linkTo(OrderController.class).withSelfRel();
+			Resources<Order> result = new Resources<Order>(orders, link);
+			return ResponseEntity.ok(result);
 		} catch (Exception e) {
-			logger.error(String.format("Erro ao executar o método GET.\nMensagem: %s",e.getMessage()));
+			getLogger().error(String.format("Erro ao executar o método GET.\nMensagem: %s", e.getMessage()));
+			return ResponseEntity.badRequest().body(null);
 		}
-		return null;
 	}
 
 	@Override
-	@GetMapping(value="/{id}",
-				consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
-	 			produces = {MediaType.APPLICATION_JSON_VALUE,
-	 						MediaType.APPLICATION_XML_VALUE,
-	 						MediaTypes.HAL_JSON_VALUE})
+	@GetMapping(value = "/{id}", consumes = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE,
+					MediaType.APPLICATION_XML_VALUE, MediaTypes.HAL_JSON_VALUE })
 	@ResponseStatus(HttpStatus.OK)
-	public Resource<Order> get(@PathVariable("id") Long id) {
+	public ResponseEntity<Resource<Order>> get(@PathVariable("id") Long id) {
 		try {
 			Order order = orderService.get(Order.builder().idOrder(id).build());
-			logger.info(String.format("Registro recuperado: %s",order.toString()));
+			getLogger().info(String.format("Registro recuperado: %s", order.toString()));
+			Link link = linkTo(OrderController.class).slash(order.getId()).withSelfRel();
+			Resource<Order> result = new Resource<Order>(order, link);
+			return ResponseEntity.ok(result);
 		} catch (Exception e) {
-			logger.error(String.format("Erro ao executar o método GET.\nMensagem: %s",e.getMessage()));
+			getLogger().error(String.format("Erro ao executar o método GET.\nMensagem: %s", e.getMessage()));
+			return ResponseEntity.badRequest().body(null);
 		}
-		return null;
 	}
 
 	@Override
-	@PatchMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void patch(@RequestBody Order order) {
-		try {
-			orderService.patch(order);
-			logger.info(String.format("Registro atualizado: %s",order.toString()));
-		} catch (Exception e) {
-			logger.error(String.format("Erro ao executar o método PATCH.\nMensagem: %s",e.getMessage()));
-		}
+	public Class<?> getClassController() {
+		return OrderController.class;
+	}
 
+	@Override
+	public GenericOperations<Order> getGenericOperations() {
+		return orderService;
 	}
 
 }
--- src/main/java/br/senaigo/mobile/interfaces/GenericOperationsController.java
+++ src/main/java/br/senaigo/mobile/interfaces/GenericOperationsController.java
@@ -1,15 +1,22 @@
 package br.senaigo.mobile.interfaces;
 
+import org.slf4j.Logger;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
 
 public interface GenericOperationsController<E> {
 	
-	Resource<E> post(E entity); //POST 201
-	void put(E entity); //PUT 204 - no content
-	void delete(E entity); //DELETE 204
-	Resources<E> get(); // GET 200
-	Resource<E> get(Long id); //GET 200
-	void patch(E entity); //PATCH 204
+	ResponseEntity<Resource<E>> post(E entity); //POST 201
+	HeadersBuilder<?> put(E entity); //PUT 204 - no content
+	HeadersBuilder<?> delete(E entity); //DELETE 204
+	ResponseEntity<Resources<E>> get(); // GET 200
+	ResponseEntity<Resource<E>> get(Long id); //GET 200
+	HeadersBuilder<?> patch(E entity); //PATCH 204
+	
+	Logger getLogger();
+	Class<?> getClassController();
+	GenericOperations<E> getGenericOperations();
 
 }
